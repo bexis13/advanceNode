5d6d27234cbe86284aa5550cf51afd38332658f6
Abose Ukhun <bexis17@gmail.com>
2019-02-18 19:25:24 +0000

    add passport for authentication and expresss-session for user session and properly configure it into app

--- server.js
+++ server.js
@@ -4,8 +4,12 @@ const express     = require('express');
 const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
 const pug = require("pug");
+const passport = require("passport");
+const session = require("express-session");
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 
 fccTesting(app); //For FCC testing purposes
 
@@ -13,6 +17,15 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine','pug');
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.route('/')
   .get((req, res) => {
